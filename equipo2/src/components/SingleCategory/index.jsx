e80213c76a66639badbf733ad7651b3c3f6fc2f0
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2021-12-24 08:24:28 +0100

    init component ProductList

---
+++ equipo2/src/components/SingleCategory/index.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+export default function TitlebarBelowImageList() {
+  return (
+    <ImageList sx={{ width: 500, height: 450 }}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={item.title}
+            subtitle={<span>by: {item.author}</span>}
+            position="below"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
