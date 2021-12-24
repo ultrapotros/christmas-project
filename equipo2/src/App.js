310a77da52afeff91741ec1e302210e193312929
Sebastian M <ra3gan1991@gmail.com>
2021-12-24 15:01:58 +0100

    We add react-router-dom and setup basic Router

--- equipo2/src/App.js
+++ equipo2/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import axios from 'axios';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+  } from "react-router-dom";
 
 function App() {
+
   return (
     <div className="App">
+      {/* <Header /> */}
+      
+       {/* <Footer /> */}
 
     </div>
   );
 }
+
 
 export default App;
