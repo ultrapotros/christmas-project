54574c2eb949b49706933b79883a96d9fd80bf30
Sebastian M <ra3gan1991@gmail.com>
2021-12-26 13:26:56 +0100

    Setup basic component

---
+++ equipo2/src/components/SingleProduct/index.jsx
@@ -0,0 +1,19 @@
+import './component.css';
+
+import React , { useState , useEffect } from 'react'
+
+function SingleProduct() {
+
+ 
+  return (
+
+    <div className="single-product">
+    
+    
+    </div>
+  
+  );
+}
+
+
+export default SingleProduct;
