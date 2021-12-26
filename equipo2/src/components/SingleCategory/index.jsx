2082dc16c76b65169037c7b21ae6d2fe195d7526
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2021-12-26 10:56:51 +0100

    product presentation settings and filtering initiation

--- equipo2/src/components/SingleCategory/index.jsx
+++ equipo2/src/components/SingleCategory/index.jsx
@@ -2,21 +2,32 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
-export default function TitlebarBelowImageList() {
+
+/**
+ * Component to display the products
+ * @returns component react
+ */
+function TitlebarBelowImageList() {
   return (
     <ImageList sx={{ width: 500, height: 450 }}>
       {itemData.map((item) => (
-        <ImageListItem key={item.img}>
+        <ImageListItem key={item.image}>
           <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.image}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
             alt={item.title}
             loading="lazy"
           />
           <ImageListItemBar
             title={item.title}
-            subtitle={<span>by: {item.author}</span>}
+            subtitle={<div className='BasicProductInformation'>
+                <p>Precio: {item.price} </p>
+                <p>Valoracion: {item.rating.rate} </p>
+              </div>}  //<span>by: {item.author}</span>}
             position="below"
           />
         </ImageListItem>
@@ -24,3 +35,44 @@ export default function TitlebarBelowImageList() {
     </ImageList>
   );
 }
+
+/**
+ * Component for ordering information
+ * @returns component react
+ */
+function CheckboxSort() {
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.value);
+  };
+
+  return (
+    <FormGroup>
+      <FormControlLabel control={<Checkbox 
+                                  onChange={handleChange} 
+                                  inputProps={ {'category': 'price'} }/>} label="Precio" />
+      <FormControlLabel control={<Checkbox 
+                                  onChange={handleChange} 
+                                  inputProps={ {'category': 'rating'} }/>} label="Valoracion" />
+    </FormGroup>
+  );
+}
+
+export default function SingleCategory() {
+  return (<div>
+      <CheckboxSort />
+      <TitlebarBelowImageList />
+    </div>
+  );
+}
+
+id
+title
+price
+description
+category
+image
+rating
+ -rate
+ -count
