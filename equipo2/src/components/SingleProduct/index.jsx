3f6e7af0577b793086fcbb72c99d59bcc0ba2183
Sebastian M <ra3gan1991@gmail.com>
2022-01-03 10:49:20 +0100

    Added loading structure and some elements

--- equipo2/src/components/SingleProduct/index.jsx
+++ equipo2/src/components/SingleProduct/index.jsx
@@ -4,20 +4,64 @@ import {Context } from "../../App";
 import React , { useState , useEffect , useContext} from 'react'
 import { useParams } from 'react-router-dom'
 
+import Divider from '@mui/material/Divider';
+import Rating from '@mui/material/Rating';
+import CircularProgress from '@mui/material/CircularProgress';
+import Skeleton from '@mui/material/Skeleton';
+import Box from '@mui/material/Box';
+
 function SingleProduct() {
+
+  //Retrieve id from the URL
   const {id} = useParams()
-  const productData = useContext(Context).filter(d=>d.id==id);
-
-  console.log("data");
+  //We take product with id from the URL
+  const productData = useContext(Context).filter(d=>d.id==id)[0];
   console.log(productData);
-  return (
+  //There are times when the component is mounted without the context with data arriving.
+  const isData = productData != undefined;
+  //We render in case we have data already loaded. In case of no data, circular load is displayed.
+  if(isData){
+    const {title,image,price,description,rating} = productData;
+    return (
 
-    <div className="single-product">
+      <div className="single-product">
+        <div className="left-side">
+          <img src={image} />
+          </div> 
+         <div className="right-side">
+           <h2>{title}</h2>
+           <Divider />
+           <div className="section-single">
+                <h3>{price}€</h3>
+                <div className="single-rating">
+                <Rating name="half-rating-read" defaultValue={rating.rate} precision={0.2} readOnly size="small"/><small> de {rating.count} votaciones.</small>
+                </div>
+                
+           </div>
+           
+           <Divider />
+           <p>{description}</p>
+           <Divider />
+
+
+           </div> 
       
+      </div>
     
-    </div>
+    );
+  }else{
+    //Structure while loading data
+    return( <>
+    <Box sx={{ width: 40 }}>
+      <CircularProgress />
+    
+      <Skeleton animation="wave" />
+      <Skeleton animation="wave" />
+      <Skeleton animation="wave" />
+    </Box>
+    </>)
+  }
   
-  );
 }
 
 
