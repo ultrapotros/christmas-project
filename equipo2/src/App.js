01143ce4c02889577f3922c92bdad52475f1c182
Sebastian M <ra3gan1991@gmail.com>
2022-01-07 18:10:10 +0100

    Fix provider

--- equipo2/src/App.js
+++ equipo2/src/App.js
@@ -1,62 +1,53 @@
-import './App.css';
-import TabsRouter from './components/Header';
-import SingleProduct from './components/SingleProduct';
-import axios from 'axios';
-import React , { useState , useEffect, createContext } from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-  } from "react-router-dom";
+import "./App.css";
+import TabsRouter from "./components/Header";
+import SingleProduct from "./components/SingleProduct";
+import axios from "axios";
+import React, { useState, useEffect, createContext } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export const Context = createContext(null);
 export const CartContext = createContext(null);
 
 function App() {
+  const [products, setProducts] = useState([]);
 
-  const [products,setProducts] = useState([]);
-
-  const [cart,setCart] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     async function fetchApi() {
-    try{
-      let response = await axios.get("https://fakestoreapi.com/products")
-      setProducts(response.data);
-    }catch (err){
-      console.log(err);
-    }    
+      try {
+        let response = await axios.get("https://fakestoreapi.com/products");
+        setProducts(response.data);
+      } catch (err) {
+        console.log(err);
+      }
     }
     fetchApi();
-
-  }, [])
+  }, []);
 
   return (
-    <Router >
-    <Context.Provider value={products}>
-    <div className="App">
+    <Router>
+      <CartContext.Provider value={{ cart, setCart }}>
+      <Context.Provider value={products}>
+        <div className="App">
+          <TabsRouter />
 
-      <TabsRouter />
-
-      <Routes >
-        <Route path="/" element={<h2>HomePage</h2>} />
-        <Route path="/single-product/:id" element={<SingleProduct />} />
-        <Route path="/single-category/:cat" 
-               element={<CartContext value={{cart,setCart}} >
-                          <h2>SingleCategory</h2>
-                        </CartContext>} />
-        <Route path="/cart" element={<h2>Cart</h2>} />
-        <Route path="/about-us" element={<h2>About us</h2>} />
-       </Routes>
-       {/* <Footer /> */}
-
-    </div>
-    </Context.Provider>
+          <Routes>
+            <Route path="/" element={<h2>HomePage</h2>} />
+            <Route path="/single-product/:id" element={<SingleProduct />} />
+            <Route
+              path="/single-category/:cat"
+              element={<h2>SingleCategory</h2>}
+            />
+            <Route path="/cart" element={<h2>Cart</h2>} />
+            <Route path="/about-us" element={<h2>About us</h2>} />
+          </Routes>
+          {/* <Footer /> */}
+        </div>
+      </Context.Provider>
+      </CartContext.Provider>
     </Router>
   );
 }
 
-
 export default App;
-
