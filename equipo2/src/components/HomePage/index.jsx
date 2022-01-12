1d2ade5e740bc214ebc2d4887af0b16ab453f27a
santi <santimezquita@hotmail.es>
2022-01-12 07:01:55 +0100

    home page showing all products

--- equipo2/src/components/HomePage/index.jsx
+++ equipo2/src/components/HomePage/index.jsx
@@ -5,7 +5,7 @@ import ImageListItem from '@mui/material/ImageListItem';
 import './component.css';
 import { Context } from '../../App';
 function srcset(image, size, rows = 1, cols = 1) {
-  let col = Math.floor(Math.random() * (5 - 2)) + 1;
+  let col = Math.floor(Math.random() * (5 - 2)) + 1; /* each time we render in a different format */
   let row = Math.floor(Math.random() * (3 - 1)) + 1;
   return {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
@@ -16,18 +16,18 @@ function srcset(image, size, rows = 1, cols = 1) {
 }
 
 export default function HomePage() {
-  const contextValue = useContext(Context);
-  return (
+  const contextValue = useContext(Context); /* getting data from context */
+      return (
     <ImageList className="imagesContainer"
-      sx={{ width: 3/4, height: 450 }}
+      sx={{ width: 3/4, height: 4/4 }} /*  giving style to the images container */
       variant="quilted"
       cols={4}
       rowHeight={121}
       gap={10}
     >
-      {contextValue.map((item) => (
-        <ImageListItem key={item.image} cols={1} rows={Math.floor(Math.random() * (3 - 1)) + 1}>
-          <img
+      {contextValue.map((item) => ( /* rendering all products */
+        <ImageListItem id="images" key={item.image} cols={1} rows={Math.floor(Math.random() * (3 - 1)) + 1}>
+          <img className="productImages"
             {...srcset(item.image, 200, item.rows, item.cols)}
             alt={item.title}
             loading="lazy"
@@ -36,6 +36,7 @@ export default function HomePage() {
       ))}
     </ImageList>
   );
+
 }
 
 
