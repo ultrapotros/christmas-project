5e0a4adc729e1d944321d6a92963e7b43fe4d841
santi <santimezquita@hotmail.es>
2022-01-13 20:55:17 +0100

    conflicts resolved??

--- equipo2/src/App.js
+++ equipo2/src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import Header from './components/Header';
+import SingleCategory from './components/SingleCategory';
+import SingleProduct from './components/SingleProduct';
 import axios from 'axios';
 import React , { useState , useEffect, createContext } from 'react'
 import {
@@ -14,9 +16,7 @@ export const CartContext = createContext(null);
 
 function App() {
   const [products, setProducts] = useState([]);
-
   const [cart, setCart] = useState([]);
-
   useEffect(() => {
     async function fetchApi() {
       try {
@@ -29,27 +29,23 @@ function App() {
     fetchApi();
   }, []);
 
-  //console.log(products);
-
   return (
     <Router >
-    <CartContext.Provider value={{ cart, setCart }}>
-    <Context.Provider value={products}>
-    <div className="App">
-      <Header /> 
-      <Routes >
-        <Route path="/" element={<h2>HomePage</h2>} />
-        <Route path="/single-category/:cat" element={<SingleCategory />} />
-        <Route path="/single-product/:id" element={<SingleProduct />} />
-        <Route path="/cart" element={<h2>Cart</h2>} />
-        <Route path="/about-us" element={<h2>About us</h2>} />
-       </Routes>
-       {/* <Footer /> */}
-
-    </div>
-    </Context.Provider>
-</CartContext.Provider>
-
+      <CartContext.Provider value={{ cart, setCart }}>
+        <Context.Provider value={products}>
+          <div className="App">
+            <Header /> 
+            <Routes >
+              <Route path="/" element={<h2>HomePage</h2>} />
+              <Route path="/single-category/:cat" element={<SingleCategory />} />
+              <Route path="/single-product/:id" element={<SingleProduct />} />
+              <Route path="/cart" element={<h2>Cart</h2>} />
+              <Route path="/about-us" element={<h2>About us</h2>} />
+            </Routes>
+            {/* <Footer /> */}
+          </div>
+        </Context.Provider>
+      </CartContext.Provider>
     </Router>
   );
 }
