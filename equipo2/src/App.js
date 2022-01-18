39cf9b97e1311503cbca699d10e41850a5be5df7
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-01-18 14:00:31 +0100

    preparing merge dev

--- equipo2/src/App.js
+++ equipo2/src/App.js
@@ -20,6 +20,7 @@ export const CartContext = createContext(null);
 function App() {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+  
   useEffect(() => {
     async function fetchApi() {
       try {
@@ -39,25 +40,26 @@ function App() {
     fetchApi();
     checkCart();
   }, []);
+
   return (
     <Router >
-         <CartContext.Provider value={{ cart, setCart }}>
-           <Context.Provider value={products}>
-            <div className="App">
-              <Header />
-              <Routes >
-                <Route path="/" element={<h2>HomePage</h2>} />
-                <Route path="/single-category/:cat" element={<SingleCategory />} />
-                <Route path="/single-product/:id" element={<SingleProduct />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/about-us" element={<h2>About us</h2>} />
-                <Route path="/privacy-policy" element={<Privacy />} />
-               </Routes>
-               <Footer /> 
-            </div>
-            </Context.Provider>
-        </CartContext.Provider>
-</Router>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <Context.Provider value={products}>
+          <div className="App">
+            <Header />
+            <Routes >
+              <Route path="/" element={<h2>HomePage</h2>} />
+              <Route path="/single-category/:cat" element={<SingleCategory />} />
+              <Route path="/single-product/:id" element={<SingleProduct />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/about-us" element={<AboutUs/>} />
+              <Route path="/privacy-policy" element={<Privacy />} />
+            </Routes>
+            <Footer /> 
+          </div>
+        </Context.Provider>
+      </CartContext.Provider>
+    </Router>
   );
 }
 
