3f441ba9cb6a22f488e0f57037bd0714a12f9f23
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-01-25 15:32:41 +0100

    doc + css

--- equipo2/src/components/NotFoundPage/index.jsx
+++ equipo2/src/components/NotFoundPage/index.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PageNotFound from './404.webp';
 
-
-class NotFoundPage extends React.Component{
-    render(){
-        return <div>
-            <img src={PageNotFound} alt='NotFoundPage' />
-            <p style={{textAlign:"center"}}>
-              <Link to="/"> Go to Home </Link>
-            </p>
-          </div>;
-    }
+/**
+   * Component to show in case of not finding the endpoint
+   * @returns component react
+   */
+ class NotFoundPage extends React.Component{
+  render(){
+      return <div>
+          <img src={PageNotFound} alt='NotFoundPage' className='NotFoundPage'
+            style={{objectFit:'contain', width:'80%', margin:'auto', display:'block'}}/>
+          <p style={{textAlign:'center'}}>
+            <Link to="/" style={{color:'#ebb032', fontSize:'x-large'}}> Go to Home </Link>
+          </p>
+        </div>;
+  }
 }
 
 export default NotFoundPage;
