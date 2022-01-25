2c1a62daeb82a4a492162188dfedeb153d8b02c5
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-01-25 15:29:42 +0100

    Check responsive + doc

--- equipo2/src/components/NotFoundPage/index.jsx
+++ equipo2/src/components/NotFoundPage/index.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PageNotFound from './404.webp';
 
-
+/**
+   * Component to show in case of not finding the endpoint
+   * @returns component react
+   */
 class NotFoundPage extends React.Component{
     render(){
         return <div>
-            <img src={PageNotFound} alt='NotFoundPage' />
-            <p style={{textAlign:"center"}}>
-              <Link to="/"> Go to Home </Link>
+            <img src={PageNotFound} alt='NotFoundPage' className='NotFoundPage'
+              style={{objectFit:'contain', width:'80%', margin:'auto', display:'block'}}/>
+            <p style={{textAlign:'center'}}>
+              <Link to="/" style={{color:'#ebb032', fontSize:'x-large'}}> Go to Home </Link>
             </p>
           </div>;
     }
