e915615404148f45315eace12388c9888f98e37b
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-01-25 20:10:15 +0100

    sterilize css

--- equipo2/src/components/NotFoundPage/index.jsx
+++ equipo2/src/components/NotFoundPage/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PageNotFound from './404.webp';
+import './component.css';
 
 /**
    * Component to show in case of not finding the endpoint
@@ -8,11 +9,10 @@ import PageNotFound from './404.webp';
    */
  class NotFoundPage extends React.Component{
   render(){
-      return <div>
-          <img src={PageNotFound} alt='NotFoundPage' className='NotFoundPage'
-            style={{objectFit:'contain', width:'80%', margin:'auto', display:'block'}}/>
-          <p style={{textAlign:'center'}}>
-            <Link to="/" style={{color:'#ebb032', fontSize:'x-large'}}> Go to Home </Link>
+      return <div className='NotFoundPage'>
+          <img src={PageNotFound} alt='NotFoundPage'/>
+          <p>
+            <Link to="/"> Go to Home </Link>
           </p>
         </div>;
   }
